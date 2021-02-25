61fe9d11ef33843e8e5ac796334dc2c565849625
nanabach5 <nanaha255@gmail.com>
2021-02-26 00:24:26 +0900

    b

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
-apt install ansible sshpass
+USER_NAME="あなたのユーザー名"
 
-# もしrootユーザーでSSH可能な場合
+apt install ansible
 
-#
+# 
+ansible-playbook install_server.yml -k --ask-become-pass
 
+ftp_server=`cat ./remote_backup.sh | grep -E '^SERVER_FTP=.*$' | sed -s `
+
+if [ ${ftp_server} == 'ftp.example.com' ]; then
+    echo "リモートバックアップ先ホストは設定されていません。"
+    echo "リモートバックアップのセットアップはスキップします。"
+else
+    ansible-playbook 
--- remote_backup.sh
+++ remote_backup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# ftp server URL or IP
+# バックアップ先FTPサーバーのURLかIP
 SERVER_FTP="ftp.example.com"
 
-# path of ftp backup dir(destination remote server)
+# バックアップ先FTPサーバーのバックアップ先フォルダのパス
 REMOTE_DIR=mc_backup/
 
-# Path of mcbe backup dir(mcbe server)
-# You don't need to change this path unless you change backup dir path in ansbile playbook.
+# バックアップ元マイクラサーバーのバックアップディレクトリ。
+# install_server.ymlのバックアップ先ディレクトリをいじっていなければ変更不要。
 LOCAL_DIR=~mc/bedrock/MCBE_Backups/Bedrock\ level_Backups
 
-# user name to login ftp server
+# ftpサーバーにログインする用のユーザーネーム
 USER=your_user_name
-# pass to login ftp server
+# FTPサーバーにログインする用のパスワード
 PASS=password
 
 /usr/bin/lftp <<END
